0d85ad3e59a526220e882fe2574bdddcec66f9e9
Jasson Rdz <ja7rodal@gmail.com>
2017-04-30 16:02:36 -0500

    create navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
